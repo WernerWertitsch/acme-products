7fdd14e62afc4096d1eb9917ec00bba017fe1169
werner <werner@c-change.eu>
2020-02-24 01:06:27 +0100

    Frontend-Time...

--- src/main/java/corp/acme/products/functions/GetClassifications.java
+++ src/main/java/corp/acme/products/functions/GetClassifications.java
@@ -1,18 +1,19 @@
 package corp.acme.products.functions;
 
 import corp.acme.common.domain.Classification;
+import corp.acme.common.domain.ClassificationRequest;
 import corp.acme.products.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.function.Function;
 
-public class GetClassifications implements Function<List<String>, List<corp.acme.common.domain.Classification>> {
+public class GetClassifications implements Function<ClassificationRequest, List<corp.acme.common.domain.Classification>> {
     @Autowired
     ProductsService productsService;
 
     @Override
-    public List<corp.acme.common.domain.Classification> apply(List<String> names) {
-        return productsService.fetchClassifications(names);
+    public List<corp.acme.common.domain.Classification> apply(ClassificationRequest classificationRequest) {
+        return productsService.fetchClassifications(classificationRequest.getSubstanceNames());
     }
 }
